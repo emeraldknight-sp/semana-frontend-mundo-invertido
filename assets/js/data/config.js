d6be5b88ceb8fcd6b0eb77003a5665c9ac70f483
David Almeida <contato.davidalmeida@outlook.com>
2022-08-27 11:09:05 -0300

    ✅ Add: Database Settings on the Firebase Platform

---
+++ assets/js/data/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAagH7DT69pKGHadZ-Wh6_YBgWUFQV_ICw",
+  authDomain: "projeto-mundo-invertido-42a61.firebaseapp.com",
+  projectId: "projeto-mundo-invertido-42a61",
+  storageBucket: "projeto-mundo-invertido-42a61.appspot.com",
+  messagingSenderId: "413675136842",
+  appId: "1:413675136842:web:1276a1129a482ad6c543b3"
+};
+
+export default firebaseConfig;
